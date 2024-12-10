88fe1a7a9d1324b66c85d79c7d25e5a0362ac734
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-10 09:22:24 -0600

    First Commit

---
+++ src/test/java/com/yael/libs/msv/commons/libs_mvs_commons/LibsMvsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yael.libs.msv.commons.libs_mvs_commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsMvsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
